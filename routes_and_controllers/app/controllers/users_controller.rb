d63ada88c323f7943b875501b272c258a7290ae7
Sebastian Sanchez <sebastian@artxcode.io>
2021-03-31 15:07:52 -0400

    completed first routes and controllers

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        users = User.all 
+        render json: users
     end
 
     def create
@@ -8,6 +9,39 @@ class UsersController < ApplicationController
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
     end
+
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else 
+             render json: user.errors.full_messages, status: :unprocessable_entity 
+        end 
+    end 
+
+
+
+    def update
+        user = User.find(params[:id])      
+        if user.update(params.require(:user).permit(:name, :email))
+            render json: user
+        else 
+             render json: user.errors.full_messages, status: :unprocessable_entity 
+        end 
+    end 
+
+
+    def destroy
+        user = User.find(params[:id])
+
+        if user.destroy
+            render json: user
+        else 
+            render json: "can't destroy this user"
+        end 
+
+    end 
 end
